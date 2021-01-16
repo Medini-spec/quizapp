c99479035ec5d83b2cafec267f0a6dfacd950fc1
Medini-spec <73520508+Medini-spec@users.noreply.github.com>
2021-01-16 22:46:10 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,39 @@
+
+// Array of object
+
+const quiz = [
+{
+	q:'Which month comes right before june ?',
+	option:['may','september','july', 'august'],
+	answer:0
+},
+
+{
+	q:'Which color is banana ?',
+	option:['red','yellow','white', 'blue'],
+	answer:1
+},
+{
+	q:'3 + 4 = 7 ?',
+	option:['true','false'],
+	answer:0
+},
+{
+	q:'what time of day do we have breakfast ?',
+	option:['In the afternoon','In the evening','In the morning'],
+	answer:2
+},
+{
+	q:'what is 2 * 6 ?',
+	option:['12','45','21', '31'],
+	answer:0
+},
+{
+	q:'who is called bubu ?',
+	option:['avaneesh','cuteboy','polarbear', 'snowbear'],
+	answer:0
+}
+
+
+]
+
